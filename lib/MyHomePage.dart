abc22eea6b65b26a236a4df72bd7129107763cd2
DenisMacrisi <denis.macrisi46@yahoo.com>
2023-12-30 14:52:05 +0200

    Second Commit

---
+++ lib/MyHomePage.dart
@@ -0,0 +1,125 @@
+import 'package:flutter/material.dart';
+import 'LogInPage.dart';
+import 'SignInPage.dart';
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        title: Center(
+          child: Text(
+            'EpicDiceEvents',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 30.0,
+              color: Colors.white, // Culoarea textului
+              shadows: [
+                Shadow(
+                  blurRadius: 10.0,
+                  color: Colors.orangeAccent,
+                  offset: Offset(0, 0),
+                ),
+                Shadow(
+                  blurRadius: 10.0,
+                  color: Colors.orangeAccent,
+                  offset: Offset(0, 0),
+                ),
+              ],
+            ),
+          ),
+        ),
+        backgroundColor: Colors.transparent,
+        elevation: 100, // Elimină umbra sub AppBar
+      ),
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('images/bg.jpg'),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(height: 125),
+                ElevatedButton(
+                  onPressed: () {
+                    // Acțiunea pentru butonul de Log In
+                    print('Buton Log In apăsat');
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => LogInPage()),
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12.5),
+                    ),
+                    elevation: 10.0,
+                    side: BorderSide(
+                      color: Colors.orangeAccent,
+                      width: 3.0,
+                    ),
+                  ),
+                  child: Container(
+                    width: 90.0,
+                    height: 50.0,
+                    alignment: Alignment.center,
+                    child: Text(
+                      'Log In',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 28.0,
+                        color: Colors.black, // Culoarea textului
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 50), // Spațiu între butoane
+                ElevatedButton(
+                  onPressed: () {
+                    // Acțiunea pentru butonul de Sign In
+                    print('Buton Sign In apăsat');
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => SignInPage()),
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12.5),
+                    ),
+                    elevation: 10.0,
+                    side: BorderSide(
+                      color: Colors.orangeAccent,
+                      width: 3.0,
+                    ),
+                  ),
+                  child: Container(
+                    width: 90.0,
+                    height: 50.0,
+                    alignment: Alignment.center,
+                    child: Text(
+                      'Sign In',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 28.0,
+                        color: Colors.black, // Culoarea textului
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/SignInPage.dart
@@ -0,0 +1,187 @@
+// sign_in_page.dart
+import 'package:flutter/material.dart';
+
+class SignInPage extends StatelessWidget {
+
+  TextEditingController _realnameController = TextEditingController();
+  TextEditingController _usernameController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+  TextEditingController _repeatPasswordController = TextEditingController();
+  TextEditingController _cityController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        title: Text('Log In',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 25.0,
+            color: Colors.white, // Culoarea textului
+            shadows: [
+              Shadow(
+                blurRadius: 10.0,
+                color: Colors.orangeAccent,
+                offset: Offset(0, 0),
+              ),
+              Shadow(
+                blurRadius: 10.0,
+                color: Colors.orangeAccent,
+                offset: Offset(0, 0),
+              ),
+            ],
+          ),
+        ),
+        backgroundColor: Colors.transparent,
+        elevation: 100,
+      ),
+      // Adăugați conținutul specific paginii LogIn aici
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('images/bg.jpg'),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Center(
+            child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(height: 80,),
+                TextField(
+                  controller: _usernameController,
+                  decoration: InputDecoration(
+                    hintText: ' Nume și Prenume',
+                    hintStyle: TextStyle(color: Colors.black,
+                        fontWeight: FontWeight.w500,
+                        fontStyle: FontStyle.italic),
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                          color: Colors.orange.shade300, width: 1.5),
+                    ),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                          color: Colors.orange.shade200, width: 3.0),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20,),
+                TextField(
+                  controller: _usernameController,
+                  decoration: InputDecoration(
+                    hintText: ' Nume Utilizator',
+                    hintStyle: TextStyle(color: Colors.black,
+                        fontWeight: FontWeight.w500,
+                        fontStyle: FontStyle.italic),
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                          color: Colors.orange.shade300, width: 1.5),
+                    ),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                          color: Colors.orange.shade200, width: 3.0),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20,),
+                TextField(
+                  controller: _passwordController,
+                  decoration: InputDecoration(
+                    hintText: ' Parola',
+                    hintStyle: TextStyle(color: Colors.black,
+                        fontWeight: FontWeight.w500,
+                        fontStyle: FontStyle.italic),
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                          color: Colors.orange.shade300, width: 1.5),
+                    ),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                          color: Colors.orange.shade200, width: 3.0),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20,),
+                TextField(
+                  controller: _repeatPasswordController,
+                  decoration: InputDecoration(
+                    hintText: ' Repeta Parola',
+                    hintStyle: TextStyle(color: Colors.black,
+                        fontWeight: FontWeight.w500,
+                        fontStyle: FontStyle.italic),
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                          color: Colors.orange.shade300, width: 1.5),
+                    ),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                          color: Colors.orange.shade200, width: 3.0),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20,),
+                TextField(
+                  controller: _cityController,
+                  decoration: InputDecoration(
+                    hintText: ' Localitate',
+                    hintStyle: TextStyle(color: Colors.black,
+                        fontWeight: FontWeight.w500,
+                        fontStyle: FontStyle.italic),
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                          color: Colors.orange.shade300, width: 1.5),
+                    ),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                          color: Colors.orange.shade200, width: 3.0),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 50),
+                // Adăugat un spațiu vertical între câmpuri
+                ElevatedButton(
+                  onPressed: () {
+                    String username = _usernameController.text;
+                    print("Username: $username");
+
+                    print("Password: ${_passwordController.text}");
+                  },
+                  style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12.5),
+                    ),
+                    elevation: 10.0,
+                    side: BorderSide(
+                      color: Colors.orangeAccent,
+                      width: 3.0,
+                    ),
+                  ),
+                  child: Container(
+                    width: 90.0,
+                    height: 50.0,
+                    alignment: Alignment.center,
+                    child: Text(
+                      'Sign In',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20.0,
+                        color: Colors.black, // Culoarea textului
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
